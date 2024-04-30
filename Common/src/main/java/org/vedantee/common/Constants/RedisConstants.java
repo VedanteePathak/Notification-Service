a17a768a9534b03d412098300306dfc4360d6d6f
Vedantee Pathak <53381462+VedanteePathak@users.noreply.github.com>
2024-04-30 20:36:11 +0530

    Delete Common directory

--- Common/src/main/java/org/vedantee/common/Constants/RedisConstants.java
+++
@@ -1,7 +0,0 @@
-package org.adrij.common.Constants;
-
-public class RedisConstants {
-    public static final String BLACKLIST_REDIS_KEY = "blacklist";
-    public static final long TIME_TO_LIVE_IN_SECONDS = 10;
-    public static final int BATCH_SIZE = 3;
-}
