958a0a4f359b4015ceefa1c2e6e09fccea004664
Vedantee Pathak <53381462+VedanteePathak@users.noreply.github.com>
2024-04-30 19:51:08 +0530

    Add files via upload

---
+++ Consumer/src/test/java/org/vedantee/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,11 @@
+package org.adrij.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
