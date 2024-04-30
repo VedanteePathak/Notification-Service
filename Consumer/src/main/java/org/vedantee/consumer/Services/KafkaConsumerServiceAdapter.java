8569b5ccbf3fb8d667012df121eb9631c1451edd
Vedantee Pathak <53381462+VedanteePathak@users.noreply.github.com>
2024-04-30 20:36:22 +0530

    Delete Consumer directory

--- Consumer/src/main/java/org/vedantee/consumer/Services/KafkaConsumerServiceAdapter.java
+++
@@ -1,6 +0,0 @@
-package org.adrij.consumer.Services;
-
-public interface KafkaConsumerServiceAdapter {
-    void consumeKafkaMessageAndSendSMS(String kafkaReceivedMessage);
-    boolean isPhoneNumberBlacklisted(String phoneNumber);
-}
--- Consumer/src/main/java/org/vedantee/consumer/Services/ThirdPartyAPIServiceAdapter.java
+++
@@ -1,6 +0,0 @@
-package org.adrij.consumer.Services;
-
-public interface ThirdPartyAPIServiceAdapter {
-    void sendSMSOkHttp(String phoneNumber, long requestId, String message);
-    void sendSMSRestTemplate(String phoneNumber, long requestId, String smsText);
-}
